9ffb3b7580a3f9c4b0d7652bf8f71499749432fe
derekSaga <derek.coelho@outlook.com.br>
2020-09-18 20:49:27 -0300

    primeiro-commit aula 18092020

---
+++ Cliente.java
@@ -0,0 +1,9 @@
+public class Cliente{
+    public static void main(String[] args){
+        Socket = sktCliente = null;
+
+        //solicita uma conexão com o servidor
+        // etapa de comunicação
+        // encerra a conexão
+    }
+}
---
+++ Servidor.java
@@ -0,0 +1,10 @@
+public class Servidor{
+    public static void main(String[] args){
+        ServerSocket sktServer = null;
+        
+        // bind - solicitar uma porta ao SO
+        // aguarda um pedido de conexão
+        // etapa de comunicação
+        // encerra a conexão
+    }
+}
