bc8ab1dc447ca2ff56262a41d29ecb1f627b346f
derekSaga <40836760+derekSaga@users.noreply.github.com>
2020-09-19 12:55:48 -0300

    terceito commit aula-18092020

--- Cliente.java
+++
@@ -1,9 +0,0 @@
-public class Cliente{
-    public static void main(String[] args){
-        Socket = sktCliente = null;
-
-        //solicita uma conexão com o servidor
-        // etapa de comunicação
-        // encerra a conexão
-    }
-}
---
+++ servidor/Servidor.java
@@ -0,0 +1,48 @@
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Servidor {
+    public static void main(String[] args) {
+        final int PORTA = 9876;
+        ServerSocket sktServer = null;
+        Socket sktCliente = null;
+        Scanner entrada;
+
+        // bind - solicitar uma porta ao SO
+        try {
+            sktServer = new ServerSocket(PORTA);
+        } catch (Exception e) {
+            System.out.println("Porta " + PORTA + " em uso.");
+            return;
+        }
+
+        // aguarda um pedido de conexão
+        try {
+            System.out.println("Aguardando conexão.");
+            sktCliente = sktServer.accept();
+            entrada = new Scanner(sktCliente.getInputStream());
+            System.out.println("Conectado com " + sktCliente.getInetAddress().getHostAddress());
+        } catch (Exception e) {
+            System.out.println("Erro no processo de conexão.");
+            return;
+        }
+
+        // etapa de comunicação
+
+        try {
+            String msg = entrada.nextLine();
+            System.out.println("Mensagem Recebida: " + msg);
+        } catch (Exception e) {
+            System.out.println("Erro durante a comunicação com o cliente.");
+        }
+        // encerra a conexão
+        try {
+            sktCliente.close();
+            sktServer.close();
+            System.out.println("Conexão encerrada");
+        } catch (Exception e) {
+            System.out.println("Erro ao encerar a conexão.");
+        }
+    }
+}
